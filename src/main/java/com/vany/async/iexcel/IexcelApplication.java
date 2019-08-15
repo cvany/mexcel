7273417003922be4d87b54e58b750e392259be04
unknown <cuiwy@4pxad.com>
2019-08-15 11:33:29 +0800

    Initial commit

---
+++ src/main/java/com/vany/async/iexcel/IexcelApplication.java
@@ -0,0 +1,13 @@
+package com.vany.async.iexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IexcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vany/async/iexcel/IexcelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vany.async.iexcel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IexcelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
